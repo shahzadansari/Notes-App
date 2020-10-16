474764eaa54bebf6f1b21581a7755decfcc1d753
Shahzad Ansari <shahzadansari03@outlook.com>
2020-10-17 00:29:19 +0500

    Persist instance state

--- app/src/main/java/com/example/notes_app/NoteActivity.java
+++ app/src/main/java/com/example/notes_app/NoteActivity.java
@@ -284,4 +284,19 @@ public class NoteActivity extends AppCompatActivity implements
     public void updateNote() {
         mNoteViewModel.updateNote(mNoteFinal);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("mode", mMode);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mMode = savedInstanceState.getInt("mode");
+        if(mMode == EDIT_MODE_ENABLED){
+            enableEditMode();
+        }
+    }
 }
